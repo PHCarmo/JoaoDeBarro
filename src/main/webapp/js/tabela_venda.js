56e46b7f313b8f0044ab77419642f1eb6a2715b4
PHCarmo <paulo.henrique.carmo@hotmail.com>
2021-03-15 21:14:57 -0300

    Exibição de Vendas

---
+++ src/main/webapp/js/tabela_venda.js
@@ -0,0 +1,28 @@
+$("a[href='#tab_vendas']").on('click', function(e){
+    e.preventDefault();
+    if(!$.fn.DataTable.isDataTable('#vendas_table')) {
+        $('#vendas_table').DataTable({
+            "language": {
+                "url" : "//cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json"
+            },
+            "columnDefs": [{
+                "targets": [0,1,2,3,4,5],
+                "className": "dt-center"
+            },{
+                "targets": [0,1,2],
+                "width": "1%"
+            },{
+                "targets": [3],
+                "width": "15%"
+            },{
+                "targets": [4],
+                "width": "8%"
+            },{
+                "targets": [5],
+                "width": "10%"
+            }],
+            "processing": true,
+            "ajax": "./json/data_venda_table.json"
+        });
+    }
+});
