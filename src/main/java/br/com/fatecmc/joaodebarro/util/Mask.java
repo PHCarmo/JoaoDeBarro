da727950e0a2ca4e8c379ca7d60ccc46e5824df2
PHCarmo <paulo.henrique.carmo@hotmail.com>
2021-04-21 19:06:39 -0300

    Adição de método para dinheiro

--- src/main/java/br/com/fatecmc/joaodebarro/util/Mask.java
+++ src/main/java/br/com/fatecmc/joaodebarro/util/Mask.java
@@ -1,10 +1,17 @@
 package br.com.fatecmc.joaodebarro.util;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Mask {
     
     public static String toCPF(String cpf){
         while(cpf.length() < 11) { cpf += "*"; }
         return cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "." + cpf.substring(6, 9) + "-" + cpf.substring(9, 11);
+    }
+    
+    public static String toMoney(double value){
+        return NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(value);
     }
     
 }
