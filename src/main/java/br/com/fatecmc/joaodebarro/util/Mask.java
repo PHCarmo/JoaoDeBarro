e4a73fb78852f44674684da91855048f7914e900
PHCarmo <paulo.henrique.carmo@hotmail.com>
2021-04-26 18:14:56 -0300

    Otimização de processamento em numberformat

--- src/main/java/br/com/fatecmc/joaodebarro/util/Mask.java
+++ src/main/java/br/com/fatecmc/joaodebarro/util/Mask.java
@@ -4,6 +4,8 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class Mask {
+    private static final NumberFormat MOEDA_BR = NumberFormat.
+            getCurrencyInstance(new Locale("pt", "BR"));
     
     public static String toCPF(String cpf){
         while(cpf.length() < 11) { cpf += "*"; }
@@ -11,7 +13,7 @@ public class Mask {
     }
     
     public static String toMoney(double value){
-        return NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(value);
+        return MOEDA_BR.format(value);
     }
     
 }
